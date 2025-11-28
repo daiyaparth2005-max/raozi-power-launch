8a4b3f4b33a92b9c34337028c57ffde814829f1e
daiyaparth2005-max <daiyaparth2005@gmail.com>
2025-11-28 11:13:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,10 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
-  },
+  host: '::',
+  port: 8080,
+  allowedHosts: ['raoz.in', 'www.raoz.in']
+},
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
